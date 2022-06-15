19b014b73f541b132d96549eda71fe8d2369ef5a
Vitto <admin@vitto.io>
2022-06-15 20:31:59 +0200

    preparing for etherscan verification

---
+++ deploy/13_verify.ts
@@ -0,0 +1,33 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy-holographed/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+
+  if (hre.network.name != 'localhost' && hre.network.name != 'localhost2') {
+    let contracts: string[] = [
+      'Holograph',
+      'HolographFactoryProxy',
+      'HolographFactory',
+      'HolographERC721',
+      'CxipERC721',
+      'PA1D',
+    ];
+    for (let i: number = 0, l: number = contracts.length; i < l; i++) {
+      let contract: string = contracts[i];
+      try {
+        await hre.run('verify:verify', {
+          address: (await hre.ethers.getContract(contract)).address,
+          constructorArguments: [],
+        });
+      } catch (error) {
+        hre.deployments.log(`Failed to verify $contract ${error}`);
+      }
+    }
+  } else {
+    hre.deployments.log('Not verifying contracts on localhost networks.');
+  }
+};
+export default func;
+func.tags = ['Verify'];
+func.dependencies = [];
