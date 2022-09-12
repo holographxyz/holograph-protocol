f2db047efbb8d1f57b70238d4cb792d3dc23e168
ACC01ADE <admin@vitto.io>
2022-09-12 23:09:29 +0200

    Update hardhat.config.ts
    Fixing typo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -74,7 +74,7 @@ const selectDeploymentSalt = (): number => {
       }
       break;
     case Environment.mainnet:
-      salt = parseInt(process.env.DEVELOP_DEPLOYMENT_SALT || '0');
+      salt = parseInt(process.env.MAINNET_DEPLOYMENT_SALT || '0');
       if (salt > 999 || salt < 0) {
         throw new Error('DEVELOP_DEPLOYMENT_SALT is out of bounds. Allowed range is [0-999]');
       }
