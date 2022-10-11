b11ddcf3b3bcdd70b56d50fdb11199f121891ced
Vitto <admin@vitto.io>
2022-10-11 06:36:00 +0200

    LZ fees

---
+++ contracts/interface/LayerZeroOverrides.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: UNLICENSED
+/*
+
+                         ┌───────────┐
+                         │ HOLOGRAPH │
+                         └───────────┘
+╔═════════════════════════════════════════════════════════════╗
+║                                                             ║
+║                            / ^ \                            ║
+║                            ~~*~~            ¸               ║
+║                         [ '<>:<>' ]         │░░░            ║
+║               ╔╗           _/"\_           ╔╣               ║
+║             ┌─╬╬─┐          """          ┌─╬╬─┐             ║
+║          ┌─┬┘ ╠╣ └┬─┐       \_/       ┌─┬┘ ╠╣ └┬─┐          ║
+║       ┌─┬┘ │  ╠╣  │ └┬─┐           ┌─┬┘ │  ╠╣  │ └┬─┐       ║
+║    ┌─┬┘ │  │  ╠╣  │  │ └┬─┐     ┌─┬┘ │  │  ╠╣  │  │ └┬─┐    ║
+║ ┌─┬┘ │  │  │  ╠╣  │  │  │ └┬┐ ┌┬┘ │  │  │  ╠╣  │  │  │ └┬─┐ ║
+╠┬┘ │  │  │  │  ╠╣  │  │  │  │└¤┘│  │  │  │  ╠╣  │  │  │  │ └┬╣
+║│  │  │  │  │  ╠╣  │  │  │  │   │  │  │  │  ╠╣  │  │  │  │  │║
+╠╩══╩══╩══╩══╩══╬╬══╩══╩══╩══╩═══╩══╩══╩══╩══╬╬══╩══╩══╩══╩══╩╣
+╠┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴╬╬┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴╬╬┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴╣
+║               ╠╣                           ╠╣               ║
+║               ╠╣                           ╠╣               ║
+║    ,          ╠╣     ,        ,'      *    ╠╣               ║
+║~~~~~^~~~~~~~~┌╬╬┐~~~^~~~~~~~~^^~~~~~~~~^~~┌╬╬┐~~~~~~~^~~~~~~║
+╚══════════════╩╩╩╩═════════════════════════╩╩╩╩══════════════╝
+     - one protocol, one bridge = infinite possibilities -
+
+
+ ***************************************************************
+
+ DISCLAIMER: U.S Patent Pending
+
+ LICENSE: Holograph Limited Public License (H-LPL)
+
+ https://holograph.xyz/licenses/h-lpl/1.0.0
+
+ This license governs use of the accompanying software. If you
+ use the software, you accept this license. If you do not accept
+ the license, you are not permitted to use the software.
+
+ 1. Definitions
+
+ The terms "reproduce," "reproduction," "derivative works," and
+ "distribution" have the same meaning here as under U.S.
+ copyright law. A "contribution" is the original software, or
+ any additions or changes to the software. A "contributor" is
+ any person that distributes its contribution under this
+ license. "Licensed patents" are a contributor’s patent claims
+ that read directly on its contribution.
+
+ 2. Grant of Rights
+
+ A) Copyright Grant- Subject to the terms of this license,
+ including the license conditions and limitations in sections 3
+ and 4, each contributor grants you a non-exclusive, worldwide,
+ royalty-free copyright license to reproduce its contribution,
+ prepare derivative works of its contribution, and distribute
+ its contribution or any derivative works that you create.
+ B) Patent Grant- Subject to the terms of this license,
+ including the license conditions and limitations in section 3,
+ each contributor grants you a non-exclusive, worldwide,
+ royalty-free license under its licensed patents to make, have
+ made, use, sell, offer for sale, import, and/or otherwise
+ dispose of its contribution in the software or derivative works
+ of the contribution in the software.
+
+ 3. Conditions and Limitations
+
+ A) No Trademark License- This license does not grant you rights
+ to use any contributors’ name, logo, or trademarks.
+ B) If you bring a patent claim against any contributor over
+ patents that you claim are infringed by the software, your
+ patent license from such contributor is terminated with
+ immediate effect.
+ C) If you distribute any portion of the software, you must
+ retain all copyright, patent, trademark, and attribution
+ notices that are present in the software.
+ D) If you distribute any portion of the software in source code
+ form, you may do so only under this license by including a
+ complete copy of this license with your distribution. If you
+ distribute any portion of the software in compiled or object
+ code form, you may only do so under a license that complies
+ with this license.
+ E) The software is licensed “as-is.” You bear all risks of
+ using it. The contributors give no express warranties,
+ guarantees, or conditions. You may have additional consumer
+ rights under your local laws which this license cannot change.
+ To the extent permitted under your local laws, the contributors
+ exclude all implied warranties, including those of
+ merchantability, fitness for a particular purpose and
+ non-infringement.
+
+ 4. (F) Platform Limitation- The licenses granted in sections
+ 2.A & 2.B extend only to the software or derivative works that
+ you create that run on a Holograph system product.
+
+ ***************************************************************
+
+*/
+
+pragma solidity 0.8.13;
+
+interface LayerZeroOverrides {
+  // @dev defaultAppConfig struct
+  struct ApplicationConfiguration {
+    uint16 inboundProofLibraryVersion;
+    uint64 inboundBlockConfirmations;
+    address relayer;
+    uint16 outboundProofType;
+    uint64 outboundBlockConfirmations;
+    address oracle;
+  }
+
+  struct DstPrice {
+    uint128 dstPriceRatio; // 10^10
+    uint128 dstGasPriceInWei;
+  }
+
+  struct DstConfig {
+    uint128 dstNativeAmtCap;
+    uint64 baseGas;
+    uint64 gasPerByte;
+  }
+
+  // @dev using this to retrieve UltraLightNodeV2 address
+  function defaultSendLibrary() external view returns (address);
+
+  // @dev using this to extract defaultAppConfig
+  function getAppConfig(uint16 destinationChainId, address userApplicationAddress)
+    external
+    view
+    returns (ApplicationConfiguration memory);
+
+  // @dev using this to extract defaultAppConfig directly from storage slot
+  function defaultAppConfig(uint16 destinationChainId)
+    external
+    view
+    returns (
+      uint16 inboundProofLibraryVersion,
+      uint64 inboundBlockConfirmations,
+      address relayer,
+      uint16 outboundProofType,
+      uint64 outboundBlockConfirmations,
+      address oracle
+    );
+
+  // @dev access the mapping to get base price fee
+  function dstPriceLookup(uint16 destinationChainId)
+    external
+    view
+    returns (uint128 dstPriceRatio, uint128 dstGasPriceInWei);
+
+  // @dev access the mapping to get base gas and gas per byte
+  function dstConfigLookup(uint16 destinationChainId, uint16 outboundProofType)
+    external
+    view
+    returns (
+      uint128 dstNativeAmtCap,
+      uint64 baseGas,
+      uint64 gasPerByte
+    );
+}
---
+++ src/interface/LayerZeroOverrides.sol
@@ -0,0 +1,64 @@
+/*HOLOGRAPH_LICENSE_HEADER*/
+
+/*SOLIDITY_COMPILER_VERSION*/
+
+interface LayerZeroOverrides {
+  // @dev defaultAppConfig struct
+  struct ApplicationConfiguration {
+    uint16 inboundProofLibraryVersion;
+    uint64 inboundBlockConfirmations;
+    address relayer;
+    uint16 outboundProofType;
+    uint64 outboundBlockConfirmations;
+    address oracle;
+  }
+
+  struct DstPrice {
+    uint128 dstPriceRatio; // 10^10
+    uint128 dstGasPriceInWei;
+  }
+
+  struct DstConfig {
+    uint128 dstNativeAmtCap;
+    uint64 baseGas;
+    uint64 gasPerByte;
+  }
+
+  // @dev using this to retrieve UltraLightNodeV2 address
+  function defaultSendLibrary() external view returns (address);
+
+  // @dev using this to extract defaultAppConfig
+  function getAppConfig(uint16 destinationChainId, address userApplicationAddress)
+    external
+    view
+    returns (ApplicationConfiguration memory);
+
+  // @dev using this to extract defaultAppConfig directly from storage slot
+  function defaultAppConfig(uint16 destinationChainId)
+    external
+    view
+    returns (
+      uint16 inboundProofLibraryVersion,
+      uint64 inboundBlockConfirmations,
+      address relayer,
+      uint16 outboundProofType,
+      uint64 outboundBlockConfirmations,
+      address oracle
+    );
+
+  // @dev access the mapping to get base price fee
+  function dstPriceLookup(uint16 destinationChainId)
+    external
+    view
+    returns (uint128 dstPriceRatio, uint128 dstGasPriceInWei);
+
+  // @dev access the mapping to get base gas and gas per byte
+  function dstConfigLookup(uint16 destinationChainId, uint16 outboundProofType)
+    external
+    view
+    returns (
+      uint128 dstNativeAmtCap,
+      uint64 baseGas,
+      uint64 gasPerByte
+    );
+}
