316a4deb00133042fee5364b9487fb1b49523964
sogoiii <sogoiii@gmail.com>
2022-10-11 17:30:46 -0700

    add init tests for interfaces

--- test/13_holograph_interfaces_tests.ts
+++ test/13_holograph_interfaces_tests.ts
@@ -1,12 +1,52 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { generateInitCode, zeroAddress } from '../scripts/utils/helpers';
+
 describe('Holograph Interfaces Contract', async function () {
+    let HolographInterfaces: any;
+    let holographInterfaces: any;
+    let accounts: SignerWithAddress[];
+    let deployer: SignerWithAddress;
+    let newDeployer: SignerWithAddress;
+    let anotherNewDeployer: SignerWithAddress;
+
+    let factoryAddr: string;
+    let holographAddr: string;
+    let operatorAddr: string;
+    let registryAddr: string;
+    let utilityTokenAddr: string;
+
+    before(async () => {
+        accounts = await ethers.getSigners();
+        deployer = accounts[0];
+        newDeployer = accounts[1];
+        anotherNewDeployer = accounts[2];
+
+        factoryAddr = '0x1Ce073E0A6912dBEAc4A1bE1C46186828fc8e0DD'; // NOTE: sample Address
+        holographAddr = '0x0Ab35331cc5130DD52e51a9014069f18b8B5EDF9'; // NOTE: sample Address
+        operatorAddr = '0xb197381F633db828a10821Ab4B6827ed5d81BC95'; // NOTE: sample Address
+        registryAddr = '0xeB721f3E4C45a41fBdF701c8143E52665e67c76b'; // NOTE: sample Address
+        utilityTokenAddr = '0x4b02422DC46bb21D657A701D02794cD3Caeb17d0'; // NOTE: sample Address
+
+        HolographInterfaces = await ethers.getContractFactory('HolographInterfaces');
+        holographInterfaces = await HolographInterfaces.deploy();
+        await holographInterfaces.deployed();
+    });
 
     describe('constructor', async function () {
         it('should successfully deploy')
     })
 
-    describe('init()', async function() {
-        it('should successfully be initialized once') // Validate hardcoded values are correct
-        it('should fail if already initialized')
+    describe.only('init()', async function() {
+        it('should successfully be initialized once', async function() {
+            const initCode = generateInitCode(['address'], [deployer.address]);
+            await expect(holographInterfaces.connect(deployer).init(initCode)).to.not.be.reverted;
+        })
+        it('should fail if already initialized', async function() {
+            const initCode = generateInitCode(['address'], [deployer.address]);
+            await expect(holographInterfaces.connect(deployer).init(initCode)).to.be.reverted;
+        });
         it('Should allow external contract to call fn')
         it('should fail to allow inherited contract to call fn')
     })
