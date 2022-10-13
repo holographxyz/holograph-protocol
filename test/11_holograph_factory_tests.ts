38875e93fa99e8644a39fd0627ecdfb9e3da3938
Alexander <alexanderattar@gmail.com>
2022-10-12 21:51:44 -0500

    Add progress on factory tests

--- test/11_holograph_factory_tests.ts
+++ test/11_holograph_factory_tests.ts
@@ -1,109 +1,122 @@
+import { PreTest } from './utils';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
+import setup from './utils';
 import { generateInitCode, zeroAddress } from '../scripts/utils/helpers';
 
-describe('Holograph Factory Contract', async function () {
-    let HolographFactory: any;
-    let holographFactory: any;
-    let accounts: SignerWithAddress[];
-    let deployer: SignerWithAddress;
-    let newDeployer: SignerWithAddress;
-    let anotherNewDeployer: SignerWithAddress;
+describe.only('Holograph Factory Contract', async () => {
+  let l1: PreTest;
+  let HolographRegistry: any;
+  let holographRegistry: any;
 
-    let factoryAddr: string;
-    let holographAddr: string;
-    let operatorAddr: string;
-    let registryAddr: string;
-    let utilityTokenAddr: string;
+  let Mock: any;
+  let mock: any;
+  let accounts: SignerWithAddress[];
+  let deployer: SignerWithAddress;
+  let newDeployer: SignerWithAddress;
+  let anotherNewDeployer: SignerWithAddress;
+  let mockSigner: SignerWithAddress;
+  let chainId: number;
 
-    before(async () => {
-        accounts = await ethers.getSigners();
-        deployer = accounts[0];
-        newDeployer = accounts[1];
-        anotherNewDeployer = accounts[2];
+  before(async () => {
+    l1 = await setup();
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    newDeployer = accounts[1];
+    anotherNewDeployer = accounts[2];
+    chainId = (await ethers.provider.getNetwork()).chainId;
 
-        factoryAddr = '0x1Ce073E0A6912dBEAc4A1bE1C46186828fc8e0DD'; // NOTE: sample Address
-        holographAddr = '0x0Ab35331cc5130DD52e51a9014069f18b8B5EDF9'; // NOTE: sample Address
-        operatorAddr = '0xb197381F633db828a10821Ab4B6827ed5d81BC95'; // NOTE: sample Address
-        registryAddr = '0xeB721f3E4C45a41fBdF701c8143E52665e67c76b'; // NOTE: sample Address
-        utilityTokenAddr = '0x4b02422DC46bb21D657A701D02794cD3Caeb17d0'; // NOTE: sample Address
+    Mock = await ethers.getContractFactory('Mock');
+    mock = await Mock.deploy();
+    await mock.deployed();
 
-        HolographFactory = await ethers.getContractFactory('HolographFactory');
-        holographFactory = await HolographFactory.deploy();
-        await holographFactory.deployed();
+    mockSigner = await ethers.getSigner(mock.address);
+  });
+
+  describe('init():', async () => {
+    // TODO: Check initialized
+    // it.only('should check that contract was successfully initialized once', async () => {
+    //   await expect(l1.holographFactory.connect(deployer)._isInitialized().to.equal(true);
+    // });
+
+    it('should fail if already initialized', async () => {
+      const initCode = generateInitCode(
+        ['address', 'address'],
+        [l1.holographFactory.address, l1.holographRegistry.address]
+      );
+      await expect(l1.holographFactory.connect(deployer).init(initCode)).to.be.reverted;
+    });
+  });
+
+  describe.skip(`bridgeIn()`, async () => {
+    it('should return the expected selector from the input payload', async () => {
+      const payload = '0x0000000000000000000000000000000000000000000000000000000000000000';
+      const expectedSelector = '0x00000000';
+      const selector = await l1.holographFactory.bridgeIn(chainId, payload);
+      expect(selector).to.equal(expectedSelector);
     });
 
-    describe('init():', async function () {
-        it('should successfully init once', async function() {
-            const initCode = generateInitCode(['address', 'address'], [holographAddr, registryAddr]);
-            await expect(holographFactory.connect(deployer).init(initCode)).to.not.be.reverted;
-        })
-        it('should fail if already initialized', async function() {
-            const initCode = generateInitCode(['address', 'address'], [holographAddr, registryAddr]);
-            await expect(holographFactory.connect(deployer).init(initCode)).to.be.reverted;
-        });
-    })
+    it('should return bad data if payload data is invalid', async () => {
+      const payload = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    });
+  });
 
-    describe(`bridgeIn()`, async function() {
-        it('should return the expected selector from the input payload')
-        it('should return bad data if payload data is invalid')
-    })
+  describe('bridgeOut()', async () => {
+    it('should return selector and payload');
+  });
 
-    describe('bridgeOut()', async function() {
-        it('should return selector and payload')
-    })
+  describe('deployHolographableContract()', async () => {
+    it('should fail with invalid signature if config is incorrect');
+    it('should fail with invalid signature if signature.r is incorrect');
+    it('should fail with invalid signature if signature.s is incorrect');
+    it('should fail with invalid signature if signature.v is incorrect');
+    it('should fail with invalid signature if signer is incorrect');
 
-    describe('deployHolographableContract()', async function() {
-        it('should fail with invalid signature if config is incorrect')
-        it('should fail with invalid signature if signature.r is incorrect')
-        it('should fail with invalid signature if signature.s is incorrect')
-        it('should fail with invalid signature if signature.v is incorrect')
-        it('should fail with invalid signature if signer is incorrect')
+    it('should fail contract was already deployed');
 
-        it('should fail contract was already deployed')
+    it('Should allow external contract to call fn');
+    it('should fail to allow inherited contract to call fn');
+  });
 
-        it('Should allow external contract to call fn')
-        it('should fail to allow inherited contract to call fn')
-    })
+  describe('setHolograph() / getHolograph()', async () => {
+    it('should allow admin to alter _holographSlot', async () => {
+      await l1.holographFactory.setHolograph(l1.holograph.address);
+      const _holographSlot = await l1.holographFactory.getHolograph();
+      expect(_holographSlot).to.equal(l1.holograph.address);
+    });
 
-    describe(`getHolograph()`, async function() {
-        it('Should return valid _holographSlot')
-        it('Should allow external contract to call fn')
-        it('should fail to allow inherited contract to call fn')
-    })
+    it('should fail to allow owner to alter _holographSlot');
+    it('should fail to allow non-owner to alter _holographSlot');
+  });
 
-    describe('setHolograph()', async function() {
-        it('should allow admin to alter _holographSlot')
-        it('should fail to allow owner to alter _holographSlot')
-        it('should fail to allow non-owner to alter _holographSlot')
-    })
+  describe('setRegistry() / getRegistry()', async () => {
+    it('should allow admin to alter _registrySlot', async () => {
+      await l1.holographFactory.setRegistry(l1.holographRegistry.address);
+      const _registrySlot = await l1.holographFactory.getRegistry();
+      expect(_registrySlot).to.equal(l1.holographRegistry.address);
+    });
 
-    describe(`getRegistry()`, async function() {
-        it('Should return valid _registrySlot')
-        it('Should allow external contract to call fn')
-        it('should fail to allow inherited contract to call fn')
-    })
+    it('should fail to allow owner to alter _registrySlot', async () => {
+      await expect(l1.holographFactory.connect(deployer).setRegistry(l1.holographRegistry.address)).to.be.reverted;
+    });
 
-    describe('setRegistry()', async function() {
-        it('should allow admin to alter _registrySlot')
-        it('should fail to allow owner to alter _registrySlot')
-        it('should fail to allow non-owner to alter _registrySlot')
-    })
+    it('should fail to allow non-owner to alter _registrySlot');
+  });
 
-    describe('_isContract()', async function() {
-        it('should not be callable')
-    })
+  describe('_isContract()', async () => {
+    it('should not be callable');
+  });
 
-    describe('_verifySigner()', async function() {
-        it('should not be callable')
-    })
+  describe('_verifySigner()', async () => {
+    it('should not be callable');
+  });
 
-    describe(`receive()`, async function() {
-        it('should revert')
-    })
+  describe(`receive()`, async () => {
+    it('should revert');
+  });
 
-    describe(`fallback()`, async function() {
-        it('should revert')
-    })
-})
+  describe(`fallback()`, async () => {
+    it('should revert');
+  });
+});
