b7faea8f916d01e516a89c3ba27e56682d82effd
Léo FASANO <fasanol.info@gmail.com>
2024-06-05 15:26:31 +0200

    test: fix wrong link

--- test/foundry/HolographDropERC721/HolographDropERC721.forks.t.sol
+++ test/foundry/HolographDropERC721/HolographDropERC721.forks.t.sol
@@ -22,7 +22,7 @@ contract HolographDropERC721Test is Test {
 
   /**
    * @dev This test reproduce the exact transaction made at block 6031723
-   *      => https://sepolia.etherscan.io/address/0xf43953DDE38d03F3feA00DD76685857E57Af49C8#code
+   *      => https://sepolia.etherscan.io/tx/0xb8dcf96afafb93d9148c1c469712ec37be17b8913a68f2c1a1d371a18d1194fb
    *      => https://dashboard.tenderly.co/tx/sepolia/0xb8dcf96afafb93d9148c1c469712ec37be17b8913a68f2c1a1d371a18d1194fb/debugger?trace=0.4.3.3.23
    * @dev To run this test you need to use a fork of sepolia at the block 6031724 and to use -vvvv flag to see
    *      the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 609888904007765 wei
@@ -50,7 +50,7 @@ contract HolographDropERC721Test is Test {
    * @dev This test reproduce the exact transaction made at block 6031723 BUT with the fixed version of the HolographDropERC721V2
    *      The HolographDropERC721V2 is at the address 0xf43953DDE38d03F3feA00DD76685857E57Af49C8, and its bytecode is replaced
    *      with the new version of the contract.
-   *      => https://sepolia.etherscan.io/address/0xf43953DDE38d03F3feA00DD76685857E57Af49C8#code
+   *      => https://sepolia.etherscan.io/tx/0xb8dcf96afafb93d9148c1c469712ec37be17b8913a68f2c1a1d371a18d1194fb
    *      => https://dashboard.tenderly.co/tx/sepolia/0xb8dcf96afafb93d9148c1c469712ec37be17b8913a68f2c1a1d371a18d1194fb/debugger?trace=0.4.3.3.23
    * @dev To run this test you need to use a fork of sepolia at the block 6031724 and to use -vvvv flag to see
    *      the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 609888904007765 wei
