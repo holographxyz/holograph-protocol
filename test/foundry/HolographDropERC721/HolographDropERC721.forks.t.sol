ecac4798783963cd54e792b2412698522b3e022d
Léo FASANO <fasanol.info@gmail.com>
2024-06-05 18:05:39 +0200

    test: add tests for other chains

--- test/foundry/HolographDropERC721/HolographDropERC721.forks.t.sol
+++ test/foundry/HolographDropERC721/HolographDropERC721.forks.t.sol
@@ -16,9 +16,37 @@ import {HolographDropERC721V2} from "src/drops/token/HolographDropERC721V2.sol";
  *      reverting with an EvmError: OutOfFunds
  */
 contract HolographDropERC721Test is Test {
+  string SEPOLIA_RPC_URL = vm.envString("SEPOLIA_RPC_URL");
+  string OP_SEPOLIA_RPC_URL = vm.envString("OP_SEPOLIA_RPC_URL");
+  string BASE_SEPOLIA_RPC_URL = vm.envString("BASE_SEPOLIA_RPC_URL");
+  string ZORA_SEPOLIA_RPC_URL = vm.envString("ZORA_SEPOLIA_RPC_URL");
+  string ARB_SEPOLIA_RPC_URL = vm.envString("ARB_SEPOLIA_RPC_URL");
+  string BNB_TESTNET_RPC_URL = vm.envString("BNB_TESTNET_RPC_URL");
+  string FUJI_RPC_URL = vm.envString("FUJI_RPC_URL");
+
+  uint256 sepoliaFork;
+  uint256 opSepoliaFork;
+  uint256 baseSepoliaFork;
+  uint256 zoraSepoliaFork;
+  uint256 arbSepoliaFork;
+  uint256 bnbTestnetFork;
+  uint256 fujiFork;
 
   function setUp() public {
+    sepoliaFork = vm.createFork(SEPOLIA_RPC_URL);
+    opSepoliaFork = vm.createFork(OP_SEPOLIA_RPC_URL);
+    baseSepoliaFork = vm.createFork(BASE_SEPOLIA_RPC_URL);
+    zoraSepoliaFork = vm.createFork(ZORA_SEPOLIA_RPC_URL);
+    arbSepoliaFork = vm.createFork(ARB_SEPOLIA_RPC_URL);
+    bnbTestnetFork = vm.createFork(BNB_TESTNET_RPC_URL);
+    fujiFork = vm.createFork(FUJI_RPC_URL);
   }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                   SEPOLIA                                  */
+  /* -------------------------------------------------------------------------- */
+
+  /* ------------------------- Reproduced transaction ------------------------- */
 
   /**
    * @dev This test reproduce the exact transaction made at block 6031723
@@ -28,7 +56,9 @@ contract HolographDropERC721Test is Test {
    *      the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 609888904007765 wei
    *      failing with an EvmError: OutOfFunds
    */
-  function test_V2PurchaseFreeMoeWithOldVersion() public {
+  function test_Sepolia_V2PurchaseFreeMoeWithOldVersion() public {
+    vm.selectFork(sepoliaFork);
+
     /// @dev To run this test you need to use a fork of sepolia at the block 6031724
     ///      You can create a fork like that using tenderly and creating a new fork at block 6031723
     assertEq(block.number, 6031724);
@@ -46,6 +76,8 @@ contract HolographDropERC721Test is Test {
     assertTrue(success);
   }
 
+  /* ---------------------------- Fixed transaction --------------------------- */
+
   /**
    * @dev This test reproduce the exact transaction made at block 6031723 BUT with the fixed version of the HolographDropERC721V2
    *      The HolographDropERC721V2 is at the address 0xf43953DDE38d03F3feA00DD76685857E57Af49C8, and its bytecode is replaced
@@ -56,7 +88,9 @@ contract HolographDropERC721Test is Test {
    *      the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 609888904007765 wei
    *      succeeding this time.
    */
-  function test_V2PurchaseFreeMoeWithNewVersion() public {
+  function test_Sepolia_V2PurchaseFreeMoeWithNewVersion() public {
+    vm.selectFork(sepoliaFork);
+      
     /// @dev To run this test you need to use a fork of sepolia at the block 6031724
     ///      You can create a fork like that using tenderly and creating a new fork at block 6031723
     assertEq(block.number, 6031724);
@@ -69,6 +103,207 @@ contract HolographDropERC721Test is Test {
     // Doing the exact same call as the previous test
     vm.prank(0xa57106357F9A487F6AfBaA3758e7fCcB787113c4);
     (bool success, bytes memory data) = address(0x731F5129F241edAc48fA088c5DE3b3149dF822FD).call{value: 609888904007765}(
+      abi.encodeWithSelector(
+        IHolographDropERC721V2.purchase.selector,
+        1
+      )
+    );
+
+    assertTrue(success);
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                              Optimism Sepolia                              */
+  /* -------------------------------------------------------------------------- */
+
+  /* ------------------------- Reproduced transaction ------------------------- */
+
+  /**
+   * @dev This test reproduce the exact transaction made at block 12809571
+   *      => https://sepolia-optimism.etherscan.io/tx/0x5b66e38dffc7afeb57ad88af3eb18f545f6ed376710f266ef4c6c65320ac200b
+   *      => https://dashboard.tenderly.co/tx/optimistic-sepolia/0x5b66e38dffc7afeb57ad88af3eb18f545f6ed376710f266ef4c6c65320ac200b
+   * @dev To run this test you need to use a fork of optimism sepolia at the block 12809571 and to use -vvvv flag to see
+   *     the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 609888904007765 wei
+   *    failing with an EvmError: OutOfFunds
+   */
+  function test_OptimismSepolia_V2PurchaseFreeMoeWithOldVersion() public {
+    vm.selectFork(opSepoliaFork);
+
+    /// @dev To run this test you need to use a fork of sepolia at the block 12809572
+    ///      You can create a fork like that using tenderly and creating a new fork at block 12809571
+    assertEq(block.number, 12809572);
+    assertEq(block.chainid, 11155420);
+
+    // Doing the exact same call as the target transaction
+    vm.prank(0xa57106357F9A487F6AfBaA3758e7fCcB787113c4);
+    (bool success, bytes memory data) = address(0x731F5129F241edAc48fA088c5DE3b3149dF822FD).call{value: 609888904007765}(
+      abi.encodeWithSelector(
+        IHolographDropERC721V2.purchase.selector,
+        1
+      )
+    );
+
+    assertTrue(success);
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                Base Sepolia                                */
+  /* -------------------------------------------------------------------------- */
+
+  /* ------------------------- Reproduced transaction ------------------------- */
+
+  /**
+   * @dev This test reproduce the exact transaction made at block 10826672
+   *      => https://sepolia.basescan.org/tx/0xafbd520ca736fb210a7e7c954ed128a0104ac6e4ee920e7c5bee3766f5b47fd5
+   * @dev To run this test you need to use a fork of base sepolia at the block 10826672 and to use -vvvv flag to see
+   *     the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 787500000000001 wei
+   *    failing with an EvmError: OutOfFunds
+   */
+  function test_BaseSepolia_V2PurchaseFreeMoeWithOldVersion() public {
+    vm.selectFork(baseSepoliaFork);
+
+    /// @dev To run this test you need to use a fork of base sepolia at the block 10826673
+    ///      You can create a fork like that using tenderly and creating a new fork at block 10826672
+    assertEq(block.number, 10826673);
+    assertEq(block.chainid, 84532);
+
+    // Doing the exact same call as the target transaction
+    vm.prank(0xa57106357F9A487F6AfBaA3758e7fCcB787113c4);
+    (bool success, bytes memory data) = address(0x731F5129F241edAc48fA088c5DE3b3149dF822FD).call{value: 787500000000001}(
+      abi.encodeWithSelector(
+        IHolographDropERC721V2.purchase.selector,
+        1
+      )
+    );
+
+    assertTrue(success);
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                Zora Sepolia                                */
+  /* -------------------------------------------------------------------------- */
+
+  /* ------------------------- Reproduced transaction ------------------------- */
+
+  /**
+   * @dev This test reproduce the exact transaction made at block 9671960
+   *      => https://sepolia.explorer.zora.energy/tx/0x13cbd3f94c33d48b6107bb87e6f27aa87ee2af86d19e59dc5d2dcfa7e731cbc3
+   * @dev To run this test you need to use a fork of zora sepolia at the block 9671960 and to use -vvvv flag to see
+   *      the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 938188474351665877 wei
+   *      failing with an EvmError: OutOfFunds
+   */
+  function test_ZoraSepolia_V2PurchaseFreeMoeWithOldVersion() public {
+    vm.selectFork(zoraSepoliaFork);
+
+    /// @dev To run this test you need to use a fork of zora sepolia at the block 9671961
+    ///      You can create a fork like that using tenderly and creating a new fork at block 9671960
+    assertEq(block.number, 9671961);
+    assertEq(block.chainid, 999999999);
+
+    vm.deal(address(0xa57106357F9A487F6AfBaA3758e7fCcB787113c4), 1000 ether);
+
+    // Doing the exact same call as the target transaction
+    vm.prank(0xa57106357F9A487F6AfBaA3758e7fCcB787113c4);
+    (bool success, bytes memory data) = address(0x731F5129F241edAc48fA088c5DE3b3149dF822FD).call{value: 938188474351665877}(
+      abi.encodeWithSelector(
+        IHolographDropERC721V2.purchase.selector,
+        1
+      )
+    );
+
+    assertTrue(success);
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                Arb Sepolia                                 */
+  /* -------------------------------------------------------------------------- */
+
+  /* ------------------------- Reproduced transaction ------------------------- */
+
+  /**
+   * @dev This test reproduce the exact transaction made at block 50820655
+   *      => https://sepolia.arbiscan.io/tx/0xc827f4b7fa301723a7c8dd5441adb6bf8b697603d91ba18602e8f1af0e329a4b
+   * @dev To run this test you need to use a fork of arb sepolia at the block 50820655 and to use -vvvv flag to see
+   *      the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 1829666914927551 wei
+   *      failing with an EvmError: OutOfFunds
+   */
+  function test_ArbSepolia_V2PurchaseFreeMoeWithOldVersion() public {
+    vm.selectFork(arbSepoliaFork);
+
+    /// @dev To run this test you need to use a fork of arb sepolia at the block 50820656
+    ///      You can create a fork like that using tenderly and creating a new fork at block 50820655
+    assertEq(block.number, 50820656);
+    assertEq(block.chainid, 421614);
+
+    // Doing the exact same call as the target transaction
+    vm.prank(0xa57106357F9A487F6AfBaA3758e7fCcB787113c4);
+    (bool success, bytes memory data) = address(0x731F5129F241edAc48fA088c5DE3b3149dF822FD).call{value: 1829666914927551}(
+      abi.encodeWithSelector(
+        IHolographDropERC721V2.purchase.selector,
+        1
+      )
+    );
+
+    assertTrue(success);
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                 BNB Testnet                                */
+  /* -------------------------------------------------------------------------- */
+
+  /* ------------------------- Reproduced transaction ------------------------- */
+
+  /**
+   * @dev This test reproduce the exact transaction made at block 40901114
+   *      => https://testnet.bscscan.com/tx/0xfc081c94a3b0c5d2ae81b1a4008d407b0668f7ed2cd55f6fa69a69137d102413
+   * @dev To run this test you need to use a fork of bnb testnet at the block 40901114 and to use -vvvv flag to see
+   *      the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 3292778604457348 wei
+   *      failing with an EvmError: OutOfFunds
+   */
+  function test_BNBTestnet_V2PurchaseFreeMoeWithOldVersion() public {
+    vm.selectFork(bnbTestnetFork);
+
+    /// @dev To run this test you need to use a fork of bnb testnet at the block 40901115
+    ///      You can create a fork like that using tenderly and creating a new fork at block 40901114
+    assertEq(block.number, 40901115);
+    assertEq(block.chainid, 97);
+
+    // Doing the exact same call as the target transaction
+    vm.prank(0xa57106357F9A487F6AfBaA3758e7fCcB787113c4);
+    (bool success, bytes memory data) = address(0x731F5129F241edAc48fA088c5DE3b3149dF822FD).call{value: 3292778604457348}(
+      abi.encodeWithSelector(
+        IHolographDropERC721V2.purchase.selector,
+        1
+      )
+    );
+
+    assertTrue(success);
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                    Fuji                                    */
+  /* -------------------------------------------------------------------------- */
+
+  /* ------------------------- Reproduced transaction ------------------------- */
+
+  /**
+   * @dev This test reproduce the exact transaction made at block 33702307
+   *      => https://testnet.avascan.info/blockchain/c/tx/0xc52733b39e6994f029ebde5ebd093d3c03983326ed7741b33deee5c4ef3ca5ab
+   * @dev To run this test you need to use a fork of fuji at the block 33702307 and to use -vvvv flag to see
+   *      the logs. You should see the transfer to 0xa57106357F9A487F6AfBaA3758e7fCcB787113c4 with a value of 191136596600000000 wei
+   *      failing with an EvmError: OutOfFunds
+   */
+  function test_Fuji_V2PurchaseFreeMoeWithOldVersion() public {
+    vm.selectFork(fujiFork);
+
+    /// @dev To run this test you need to use a fork of fuji at the block 33702308
+    ///      You can create a fork like that using tenderly and creating a new fork at block 33702307
+    assertEq(block.number, 33702308);
+    assertEq(block.chainid, 43113);
+
+    // Doing the exact same call as the target transaction
+    vm.prank(0xa57106357F9A487F6AfBaA3758e7fCcB787113c4);
+    (bool success, bytes memory data) = address(0x731F5129F241edAc48fA088c5DE3b3149dF822FD).call{value: 191136596600000000}(
       abi.encodeWithSelector(
         IHolographDropERC721V2.purchase.selector,
         1
