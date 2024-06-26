1258a77ceba8b92e5fa5430e2a1459ca13647108
Alexander <alexanderattar@gmail.com>
2024-06-26 15:48:12 -0400

    Apply updates to the HolographOperator

--- contracts/HolographOperator.sol
+++ contracts/HolographOperator.sol
@@ -1,103 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
-/*
-
-                         ┌───────────┐
-                         │ HOLOGRAPH │
-                         └───────────┘
-╔═════════════════════════════════════════════════════════════╗
-║                                                             ║
-║                            / ^ \                            ║
-║                            ~~*~~            ¸               ║
-║                         [ '<>:<>' ]         │░░░            ║
-║               ╔╗           _/"\_           ╔╣               ║
-║             ┌─╬╬─┐          """          ┌─╬╬─┐             ║
-║          ┌─┬┘ ╠╣ └┬─┐       \_/       ┌─┬┘ ╠╣ └┬─┐          ║
-║       ┌─┬┘ │  ╠╣  │ └┬─┐           ┌─┬┘ │  ╠╣  │ └┬─┐       ║
-║    ┌─┬┘ │  │  ╠╣  │  │ └┬─┐     ┌─┬┘ │  │  ╠╣  │  │ └┬─┐    ║
-║ ┌─┬┘ │  │  │  ╠╣  │  │  │ └┬┐ ┌┬┘ │  │  │  ╠╣  │  │  │ └┬─┐ ║
-╠┬┘ │  │  │  │  ╠╣  │  │  │  │└¤┘│  │  │  │  ╠╣  │  │  │  │ └┬╣
-║│  │  │  │  │  ╠╣  │  │  │  │   │  │  │  │  ╠╣  │  │  │  │  │║
-╠╩══╩══╩══╩══╩══╬╬══╩══╩══╩══╩═══╩══╩══╩══╩══╬╬══╩══╩══╩══╩══╩╣
-╠┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴╬╬┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴╬╬┴┴┴┴┴┴┴┴┴┴┴┴┴┴┴╣
-║               ╠╣                           ╠╣               ║
-║               ╠╣                           ╠╣               ║
-║    ,          ╠╣     ,        ,'      *    ╠╣               ║
-║~~~~~^~~~~~~~~┌╬╬┐~~~^~~~~~~~~^^~~~~~~~~^~~┌╬╬┐~~~~~~~^~~~~~~║
-╚══════════════╩╩╩╩═════════════════════════╩╩╩╩══════════════╝
-     - one protocol, one bridge = infinite possibilities -
-
-
- ***************************************************************
-
- DISCLAIMER: U.S Patent Pending
-
- LICENSE: Holograph Limited Public License (H-LPL)
-
- https://holograph.xyz/licenses/h-lpl/1.0.0
-
- This license governs use of the accompanying software. If you
- use the software, you accept this license. If you do not accept
- the license, you are not permitted to use the software.
-
- 1. Definitions
-
- The terms "reproduce," "reproduction," "derivative works," and
- "distribution" have the same meaning here as under U.S.
- copyright law. A "contribution" is the original software, or
- any additions or changes to the software. A "contributor" is
- any person that distributes its contribution under this
- license. "Licensed patents" are a contributor’s patent claims
- that read directly on its contribution.
-
- 2. Grant of Rights
-
- A) Copyright Grant- Subject to the terms of this license,
- including the license conditions and limitations in sections 3
- and 4, each contributor grants you a non-exclusive, worldwide,
- royalty-free copyright license to reproduce its contribution,
- prepare derivative works of its contribution, and distribute
- its contribution or any derivative works that you create.
- B) Patent Grant- Subject to the terms of this license,
- including the license conditions and limitations in section 3,
- each contributor grants you a non-exclusive, worldwide,
- royalty-free license under its licensed patents to make, have
- made, use, sell, offer for sale, import, and/or otherwise
- dispose of its contribution in the software or derivative works
- of the contribution in the software.
-
- 3. Conditions and Limitations
-
- A) No Trademark License- This license does not grant you rights
- to use any contributors’ name, logo, or trademarks.
- B) If you bring a patent claim against any contributor over
- patents that you claim are infringed by the software, your
- patent license from such contributor is terminated with
- immediate effect.
- C) If you distribute any portion of the software, you must
- retain all copyright, patent, trademark, and attribution
- notices that are present in the software.
- D) If you distribute any portion of the software in source code
- form, you may do so only under this license by including a
- complete copy of this license with your distribution. If you
- distribute any portion of the software in compiled or object
- code form, you may only do so under a license that complies
- with this license.
- E) The software is licensed “as-is.” You bear all risks of
- using it. The contributors give no express warranties,
- guarantees, or conditions. You may have additional consumer
- rights under your local laws which this license cannot change.
- To the extent permitted under your local laws, the contributors
- exclude all implied warranties, including those of
- merchantability, fitness for a particular purpose and
- non-infringement.
-
- 4. (F) Platform Limitation- The licenses granted in sections
- 2.A & 2.B extend only to the software or derivative works that
- you create that run on a Holograph system product.
-
- ***************************************************************
-
-*/
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.13;
 
@@ -194,12 +95,12 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
   /**
    * @dev Internal mapping of operator job details for a specific job hash
    */
-  mapping(bytes32 => uint256) private _operatorJobs;
+  mapping(bytes32 => uint256) private _deprecatedOperatorJobs;
 
   /**
    * @dev Internal mapping of operator job details for a specific job hash
    */
-  mapping(bytes32 => bool) private _failedJobs;
+  mapping(bytes32 => bool) private _deprecatedFailedJobs;
 
   /**
    * @dev Internal mapping of operator addresses, used for temp storage when defining an operator job
@@ -230,6 +131,16 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
    * @dev Internal mapping of bonded operator amounts
    */
   mapping(address => uint256) private _bondedAmounts;
+
+  /**
+   * @dev Internal mapping of operator job details for a specific job hash (version 2)
+   */
+  mapping(bytes32 => uint256) private _operatorJobsV2;
+
+  /**
+   * @dev Internal mapping of operator job details for a specific failed job hash (version 2)
+   */
+  mapping(bytes32 => bool) private _failedJobsV2;
 
   /**
    * @dev Constructor is left empty and init is used instead
@@ -281,16 +192,67 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
   }
 
   /**
-   * @notice Recover failed job
-   * @dev If a job fails, it can be manually recovered
-   * @param bridgeInRequestPayload the entire cross chain message payload
+   * @dev Deletes a job from the _deprecatedOperatorJobs mapping.
+   * @param jobHash The hash of the job to delete.
    */
-  function recoverJob(bytes calldata bridgeInRequestPayload) external payable {
-    bytes32 hash = keccak256(bridgeInRequestPayload);
-    require(_failedJobs[hash], "HOLOGRAPH: invalid recovery job");
-    (bool success, ) = _bridge().call{value: msg.value}(bridgeInRequestPayload);
-    require(success, "HOLOGRAPH: recovery failed");
-    delete (_failedJobs[hash]);
+  function deleteOperatorJob(bytes32 jobHash) external onlyAdmin {
+    if (_deprecatedOperatorJobs[jobHash] == 0) {
+      revert JobDoesNotExist(jobHash);
+    }
+    delete _deprecatedOperatorJobs[jobHash];
+  }
+
+  /**
+   * @dev Deletes a job from the _deprecatedFailedJobs mapping.
+   * @param jobHash The hash of the job to delete.
+   */
+  function deleteFailedJob(bytes32 jobHash) external onlyAdmin {
+    if (!_deprecatedFailedJobs[jobHash]) {
+      revert JobDoesNotExist(jobHash);
+    }
+    delete _deprecatedFailedJobs[jobHash];
+  }
+
+  /**
+   * @dev Deletes multiple jobs from the _deprecatedOperatorJobs mapping.
+   * @param jobHashes An array of job hashes to delete.
+   */
+  function deleteMultipleOperatorJobs(bytes32[] memory jobHashes) external onlyAdmin {
+    for (uint256 i = 0; i < jobHashes.length; i++) {
+      if (_deprecatedOperatorJobs[jobHashes[i]] == 0) {
+        revert JobDoesNotExist(jobHashes[i]);
+      }
+      delete _deprecatedOperatorJobs[jobHashes[i]];
+    }
+  }
+
+  /**
+   * @dev Deletes multiple jobs from the _deprecatedFailedJobs mapping.
+   * @param jobHashes An array of job hashes to delete.
+   */
+  function deleteMultipleFailedJobs(bytes32[] memory jobHashes) external onlyAdmin {
+    for (uint256 i = 0; i < jobHashes.length; i++) {
+      if (!_deprecatedFailedJobs[jobHashes[i]]) {
+        revert JobDoesNotExist(jobHashes[i]);
+      }
+      delete _deprecatedFailedJobs[jobHashes[i]];
+    }
+  }
+
+  /**
+   * @dev Checks if an operator job exists.
+   * @param jobHash The hash of the job to check.
+   */
+  function operatorJobExists(bytes32 jobHash) external view returns (bool) {
+    return _operatorJobsV2[jobHash] > 0;
+  }
+
+  /**
+   * @dev Checks if a failed job exists.
+   * @param jobHash The hash of the job to check.
+   */
+  function failedJobExists(bytes32 jobHash) external view returns (bool) {
+    return _deprecatedFailedJobs[jobHash];
   }
 
   /**
@@ -299,6 +261,8 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
    * @param bridgeInRequestPayload the entire cross chain message payload
    */
   function executeJob(bytes calldata bridgeInRequestPayload) external payable {
+    revert("HOLOGRAPH: executeJob is currently disabled");
+
     /**
      * @dev derive the payload hash for use in mappings
      */
@@ -306,7 +270,7 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
     /**
      * @dev check that job exists
      */
-    require(_operatorJobs[hash] > 0, "HOLOGRAPH: invalid job");
+    require(_operatorJobsV2[hash] > 0, "HOLOGRAPH: invalid job");
     uint256 gasLimit = 0;
     uint256 gasPrice = 0;
     assembly {
@@ -326,7 +290,7 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
     /**
      * @dev to prevent replay attacks, remove job from mapping
      */
-    delete _operatorJobs[hash];
+    delete _operatorJobsV2[hash];
     /**
      * @dev operators of last resort are allowed, but they will not receive HLG rewards of any sort
      */
@@ -448,7 +412,7 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
     } catch {
       /// @dev return any payed funds in case of revert
       payable(msg.sender).transfer(msg.value);
-      _failedJobs[hash] = true;
+      _failedJobsV2[hash] = true;
       emit FailedOperatorJob(hash);
     }
   }
@@ -497,10 +461,7 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
    * @dev This function is restricted for use by Holograph Messaging Module only
    */
   function crossChainMessage(bytes calldata bridgeInRequestPayload) external payable {
-    require(
-      msg.sender == address(_messagingModule()) || msg.sender == 0x777C19834a1A2FF6353a1E9cfb7C799ed7943a11,
-      "HOLOGRAPH: messaging only call"
-    );
+    require(msg.sender == address(_messagingModule()), "HOLOGRAPH: messaging only call");
     uint256 gasPrice = 0;
     assembly {
       /**
@@ -547,7 +508,7 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
       if (podSize > 1) {
         podSize--;
       }
-      _operatorJobs[jobHash] = uint256(
+      _operatorJobsV2[jobHash] = uint256(
         ((pod + 1) << 248) |
           (uint256(_operatorTempStorageCounter) << 216) |
           (block.number << 176) |
@@ -712,7 +673,7 @@ contract HolographOperator is Admin, Initializable, HolographOperatorInterface {
    * @return an OperatorJob struct with details about a specific job
    */
   function getJobDetails(bytes32 jobHash) public view returns (OperatorJob memory) {
-    uint256 packed = _operatorJobs[jobHash];
+    uint256 packed = _operatorJobsV2[jobHash];
     /**
      * @dev The job is bitwise packed into a single 32 byte slot, this unpacks it before returning the struct
      */
