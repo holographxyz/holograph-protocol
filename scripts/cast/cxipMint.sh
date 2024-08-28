d37aaf5c1e4f7fe9e008441a1ade57dd930a66d5
Léo FASANO <fasanol.info@gmail.com>
2024-08-28 18:58:34 +0200

    fix: cxipMint script

--- scripts/cast/cxipMint.sh
+++ scripts/cast/cxipMint.sh
@@ -2,4 +2,4 @@
 
 source .env
 
-cast send $ERC721_ADDRES --private-key $ERC721_OWNER --rpc-url $OPTIMISM_TESTNET_SEPOLIA_RPC_URL "cxipMint(uint256,uint8,string)" 0 2 "https://www.alter-a.com/wp-content/uploads/2019/07/Test-Logo-Small-Black-transparent-1.png" 
+cast send $ERC721_ADDRESS --private-key $ERC721_OWNER --rpc-url $OPTIMISM_TESTNET_SEPOLIA_RPC_URL "cxipMint(uint224,uint8,string)" 1 2 "https://www.alter-a.com/wp-content/uploads/2019/07/Test-Logo-Small-Black-transparent-1.png" 
